7cfc4c28ed9e183d7ec7d9609c8e5668eccdcbe8
YevheniiSych <66691210+YevheniiSych@users.noreply.github.com>
2020-08-21 15:25:30 +0300

    Initial commit

---
+++ app/src/main/java/elit/express/elitexpress/MainActivity.java
@@ -0,0 +1,62 @@
+package elit.express.elitexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isNetworkAvailable()) {
+//            Intent intent = new Intent("RatesActivity");
+//            startActivity(intent);
+            finish();
+        } else {
+            AlertDialog.Builder aBuilder = new AlertDialog.Builder(this);
+            aBuilder.setMessage(R.string.checkInternet)
+                    .setCancelable(true)
+                    .setNegativeButton(R.string.ok,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            });
+            AlertDialog alert = aBuilder.create();
+            alert.setTitle(R.string.connectionError);
+            alert.show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivity = (ConnectivityManager) this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity == null) {
+            return false;
+        }
+
+        // get network info for all of the data interfaces (e.g. WiFi, 3G, LTE, etc.)
+        NetworkInfo[] info = connectivity.getAllNetworkInfo();
+
+        // make sure that there is at least one interface to test against
+        // iterate through the interfaces
+        for (NetworkInfo networkInfo : info) {
+            // check this interface for a connected state
+            if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
