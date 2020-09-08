f75821db32573cb7b5b76bea548f54a0bf36a680
YevheniiSych <66691210+YevheniiSych@users.noreply.github.com>
2020-09-08 14:47:24 +0300

    registration with Constraint layout

--- app/src/main/java/elit/express/elit/RegistrationActivity.java
+++ app/src/main/java/elit/express/elit/RegistrationActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -133,5 +134,10 @@ public class RegistrationActivity extends AppCompatActivity {
 
     public static String getName() {
         return name;
+    }
+
+    private void setImageSize(){
+        ImageView image=findViewById(R.id.imageView2);
+
     }
 }
