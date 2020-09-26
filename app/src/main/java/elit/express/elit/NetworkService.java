50a3f42cc5d32768663e5d8cbcb6b57c5ee9dbd1
Roman Radko <radkoroman@gmail.com>
2020-09-26 23:14:02 +0300

    2.1

--- app/src/main/java/elit/express/elit/NetworkService.java
+++ app/src/main/java/elit/express/elit/NetworkService.java
@@ -5,14 +5,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class NetworkService {
-    private static final String BASE_URL = "https://test.sumy.kiev.ua";
-//    private static final String BASE_URL = "http://192.168.0.101";
+    private static final String BASE_URL = "https://m.sumy.kiev.ua";
+    private static final String TEST_URL = "https://test.sumy.kiev.ua";
+    //    private static final String BASE_URL = "http://192.168.0.101";
     private static NetworkService mInstance;
     private Retrofit mRetrofit;
 
     private NetworkService() {
         mRetrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(BuildConfig.DEBUG ? TEST_URL : BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
